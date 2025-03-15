1e1d84b92909ba8568f8d94f8e7519a8050dc658
pumucklkopf <120374848+pumucklkopf@users.noreply.github.com>
2025-03-15 02:32:01 +0100

    clean-up

--- main.py
+++ main.py
@@ -1,9 +1,1 @@
-from dotenv import load_dotenv
-import os
-
-# Load environment variables from .env file
-load_dotenv()
-
-# Access the environment variables
-api_key = os.getenv('SAIA_API_KEY')
-
+# Currently, functions are directly called from modules/
